dce7d6787d248f5ca34df5aff097632ba4c81ab4
i.vasilevskiy <i.vasilevskiy@cardsmobile.ru>
2019-12-14 07:29:47 +0300

    try TC

--- src/test/kotlin/ru/kc4kt4/students/test/TestApplicationTests.kt
+++ src/test/kotlin/ru/kc4kt4/students/test/TestApplicationTests.kt
@@ -1,11 +1,14 @@
 package ru.kc4kt4.students.test
 
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.web.client.RestTemplate
+import org.testcontainers.containers.GenericContainer
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext
@@ -17,6 +20,26 @@ abstract class TestApplicationTests {
     @Test
     fun contextLoads() {
     }
+
+    companion object {
+        fun mongo(): KGenericContainer {
+            return KGenericContainer("mongo:3.6").withExposedPorts(27017).withEnv("MONGO_INITDB_DATABASE", "vasilevskiy")
+        }
+
+        @BeforeAll
+        @JvmStatic
+        fun init() {
+            mongo().start()
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun stop() {
+            mongo().stop()
+        }
+
+    }
 }
 
 inline fun <reified T> typeReference() = object : ParameterizedTypeReference<T>() {}
+class KGenericContainer(imageName: String) : GenericContainer<KGenericContainer>(imageName)
